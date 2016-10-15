15955d23987b808a0817d5664115658c653c91d4
Dave Rolsky <autarch@urth.org>
2016-10-15 15:38:42 -0500

    Fix broken call to ->is_realpath

--- lib/Specio/Library/Path/Tiny.pm
+++ lib/Specio/Library/Path/Tiny.pm
@@ -31,7 +31,7 @@ my $not_absolute = sub {
 };
 
 my $not_real = sub {
-    return $_[0]->is_realpath ne $_[0] ? q{} : "$_[0] is not a real path";
+    return $_[0]->realpath ne $_[0] ? q{} : "$_[0] is not a real path";
 };
 
 my $not_file = sub {
